a661f5774f5d37eeb5ce8bb0e4e7ac0795c6dcd2
djharten <djharten@millersville.edu>
2019-01-29 00:21:32 -0500

    Camera control script added.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerController.instance.transform;
+    }
+
+    // LateUpdate is called once per frame - LU goes after Update, this is to eliminate camera lag.
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
